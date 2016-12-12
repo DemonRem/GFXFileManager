d3039918bd471d47dc5a3b7455c73ce640c1ee83
florian0 <florian0@project-hazard.org>
2016-12-12 02:08:06 +0100

    Move GFXDllCreateObject to new file
    Little cleanup

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -8,12 +8,6 @@
 
 HMODULE hInstance;
 
-#define EXPORT extern "C" __declspec(dllexport) 
-
-#define TARGET_VERSION 0x1007
-#define MODE_ARCHIVE 1
-#define MODE_FILESYSTEM 2
-
 extern FILE *dbgfile;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ulReason, LPVOID lpReserved) {
@@ -38,30 +32,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ulReason, LPVOID lpReserved) {
 	return TRUE;
 }
 
-EXPORT int __stdcall  GFXDllCreateObject(int mode, IFileManager** object, int version) {
-	char message[100];
-
-	debug(DEBUG_OBJECT, "GFXDllCreateObject(%08x, %08x, %04x)\n", mode, object, version);
-	//MessageBox(0, "CreateObject called", "", MB_OK);
-
-	if (version != TARGET_VERSION) {
-		sprintf(message, "Dll Version(%x)\nNecessary Version (%x)", version, TARGET_VERSION);
-		MessageBox(0, message, "Invalid Version(GFXFileManager.dll)", MB_OK|MB_APPLMODAL);
-		return 0;
-	}
-
-	if (mode == MODE_ARCHIVE) {
-		*object = new CPFileManager();
-	} else if (mode == MODE_FILESYSTEM) {
-		*object = new CPFileManager();
-	} else {
-		*object = 0;
-	}
-
-	return 0;
-}
-
-EXPORT void __stdcall GFXDllReleaseObject(IFileManager* object) {
+extern "C" __declspec(dllexport) void __stdcall GFXDllReleaseObject(IFileManager* object) {
 	debug(DEBUG_OBJECT, "GFXDllReleaseObject(%08x)\n", object);
 	delete object;
 }
